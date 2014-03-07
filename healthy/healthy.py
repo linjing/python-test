16bc115b880538e29dd0ffae99c00870c8042373
linjing <linjingljlj@gmail.com>
2014-03-07 13:16:18 +0800

    add healthy

---
+++ healthy/healthy.py
@@ -0,0 +1,50 @@
+import time
+
+def consume_cal(weight, height, age):
+  return 655.096 + 9.563 * weight + 1.85 * height - 4.676 * age
+
+
+class name():
+  def __init__(self, func):
+    self.__name = "linjing"
+    self.__func = func
+  def __call__(self):
+    print self.__name,
+    return self.__func
+
+def add_time(func):
+  def wrap_arg(*arg):
+    print "%s," % time.strftime("%Y%m%d"),
+    func(*arg)
+  return wrap_arg
+
+def add_name(name):
+  def wrap_fun(func):
+    def wrap_arg(*arg):
+      print "%s, " % name,
+      func (*arg)
+    return wrap_arg
+  return wrap_fun
+
+
+def show_bmi_info(func):
+  def wrap(weight, height):
+    t = func(weight, height)
+    print "your bmi = %s," % t,
+    if t > 27:
+      print "too fat"
+    elif t > 23.9:
+      print "too heavy"
+    elif t > 18.5:
+      print "normal"
+    else:
+      print "too light"
+  return wrap
+
+@add_time
+@add_name("linjing")
+@show_bmi_info
+def bmi(weigth, height):
+  return 1.0 * weigth / (height ** 2)
+
+bmi(86, 1.72)
