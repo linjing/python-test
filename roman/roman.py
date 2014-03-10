34a894b648af74e1117b5348c25a0ba36f3769f1
linjing <linjingljlj@gmail.com>
2014-03-10 15:34:55 +0800

    update

--- roman/roman.py
+++ roman/roman.py
@@ -39,8 +39,26 @@ def to_roman(n):
 
 
 import re
-romanNumeralPattern = '^M?M?M?(CM|CD|D?C?C?C?)(XC|XL|L?X?X?X?)(IX|IV|V?I?I?I?)$'
-compiledPattern = re.compile(romanNumeralPattern)
+#romanNumeralPattern = '^M?M?M?(CM|CD|D?C?C?C?)(XC|XL|L?X?X?X?)(IX|IV|V?I?I?I?)$'
+#compiledPattern = re.compile(romanNumeralPattern)
+compiledPattern = re.compile('''
+    ^
+    # beginning of string
+    M{0,3}
+    # thousands - 0 to 3 M's
+    (CM|CD|D?C{0,3})
+    # hundreds - 900 (CM), 400 (CD), 0-300 (0 to 3 C's),
+    # or 500-800 (D, followed by 0 to 3 C's)
+    (XC|XL|L?X{0,3})
+    # tens - 90 (XC), 40 (XL), 0-30 (0 to 3 X's),
+    # or 50-80 (L, followed by 0 to 3 X's)
+    (IX|IV|V?I{0,3})
+    # ones - 9 (IX), 4 (IV), 0-3 (0 to 3 I's),
+    # or 5-8 (V, followed by 0 to 3 I's)
+    $
+    # end of string
+''', re.VERBOSE)
+
 
 def from_roman(s):
   ''' return 1~3999 integer 
