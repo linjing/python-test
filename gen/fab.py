c4ee8905b03282a581bdf54e007ef8b4efff0e39
linjing <linjingljlj@gmail.com>
2014-03-11 10:42:24 +0800

    add class Fab

--- gen/fab.py
+++ gen/fab.py
@@ -9,7 +9,23 @@ def fab(max):
     n = n + 1
   return l
 
+class Fab(object):
+  def __init__(self, max):
+    self.max = max
+    self.n, self.a, self.b = 0, 0, 1
 
+  def __iter__(self):
+    return self
+
+  def next(self):
+    if self.n < self.max:
+      r = self.b
+      self.a, self.b = self.b, self.a + self.b
+      self.n += 1
+      return r
+    raise StopIteration()
 
 if __name__ == '__main__':
   print fab(5)
+  for i in Fab (10):
+    print i
