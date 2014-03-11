4f3a00ea84397c3023041c174e98c87c94774f0c
linjing <linjingljlj@gmail.com>
2014-03-11 10:37:48 +0800

    add file

---
+++ gen/fab.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+def fab(max):
+  n, a, b = 0, 0, 1
+  l = []
+  while n < max:
+    l.append(b)
+    a, b = b, a + b
+    n = n + 1
+  return l
+
+
+
+if __name__ == '__main__':
+  print fab(5)
